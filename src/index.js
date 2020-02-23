b2946baa18a5fe9e8a66dc1c0df01d3af7ee049e
Yanik Kumar <believemasters@gmail.com>
2020-02-23 21:13:36 +0530

    React Redux state changer

--- src/index.js
+++ src/index.js
@@ -10,10 +10,20 @@ import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import { BrowserRouter as Router, Switch, Route, withRouter} from 'react-router-dom';
 
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser } from "./actions"
+
+const store = createStore(rootReducer, composeWithDevTools())
+
 class Root extends React.Component {
     componentDidMount() {
         firebase.auth().onAuthStateChanged(user => {
             if(user){
+                // console.log(user);
+                this.props.setUser(user);
                 this.props.history.push('/');
             }
         })
@@ -30,12 +40,14 @@ class Root extends React.Component {
     }
 } 
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, {setUser})(Root));
 
 
 ReactDOM.render(
+<Provider store={store}>
 <Router>
 <RootWithAuth />
 </Router>
+</Provider>
 , document.getElementById('root'));
 registerServiceWorker();
