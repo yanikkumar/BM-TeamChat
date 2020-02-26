5bde9386918b58f018fbc2fb1db1a365c1bf8297
Yanik Kumar <believemasters@gmail.com>
2020-02-26 22:22:33 +0530

    Added Channel message sending feature with user

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Segment, Comment } from 'semantic-ui-react';
+import MessagesHeader from './MessagesHeader';
+import MessageForm from './MessageForm';
+
+import firebase from '../../firebase';
 
 class Messages extends React.Component {
+    state = {
+        messagesRef: firebase.database().ref('messages'),
+        channel: this.props.currentChannel,
+        user: this.props.currentUser
+    }
+
     render() {
+        const { messagesRef, channel, user } = this.state;
         return(
-            <div>Messages</div>
+            <React.Fragment>
+                <MessagesHeader />
+
+                <Segment>
+                    <Comment.Group className="messages">
+                        {/* Messages */}
+                    </Comment.Group>
+                </Segment>
+
+                <MessageForm 
+                    messagesRef={messagesRef}
+                    currentChannel={channel}
+                    currentUser={user}
+                />
+            </React.Fragment>
         )
     }
 }
