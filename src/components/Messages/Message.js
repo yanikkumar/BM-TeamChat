115ec0a726f41a0a03deba90f56e333d8fb72182
Yanik Kumar <believemasters@gmail.com>
2020-02-27 23:05:28 +0530

    Image file upload feature

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import moment from 'moment';
-import { Comment } from 'semantic-ui-react';
+import { Comment, Image } from 'semantic-ui-react';
 
 const isOwnMessage = (message, user) => {
     return message.user.id === user.uid ? 'message__self' : '';
+}
+
+const isImage = (message) => {
+    return message.hasOwnProperty('image') && !message.hasOwnProperty('content');
 }
 
 const timeFromNow = timestamp => moment(timestamp).fromNow();
@@ -13,8 +17,11 @@ const Message = ({message, user}) => (
         <Comment.Avatar src={message.user.avatar} />
         <Comment.Content className={isOwnMessage(message, user)}>
             <Comment.Author as="a">{message.user.name}</Comment.Author>
-            <Comment.Metadata>{timeFromNow(message.timestamp)}</Comment.Metadata>
-            <Comment.Text>{message.content}</Comment.Text>
+            <Comment.Metadata>{timeFromNow(message.timestamp)}</Comment.Metadata>             
+            {isImage(message) ?
+                 <Image src={message.image} className="message__image" /> :
+                 <Comment.Text>{message.content}</Comment.Text> 
+                }
         </Comment.Content>
     </Comment>
 );
