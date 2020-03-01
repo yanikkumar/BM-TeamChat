67ebe20bc42a9551cd338212966d21143c5593dc
Yanik Kumar <believemasters@gmail.com>
2020-03-01 17:44:13 +0530

    Adding private chat functionality

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import firebase from '../../firebase';
+import { connect } from 'react-redux';
+import { setCurrentChannel, setPrivateChannel } from '../../actions';
 import { Menu, Icon } from 'semantic-ui-react';
 
 class DierctMessages extends React.Component {
@@ -68,6 +70,21 @@ class DierctMessages extends React.Component {
 
     isUserOnline = user => user.status === 'online';
     
+    changeChannel = user => {
+        const channelId = this.getChannelId(user.uid);
+        const channelData = {
+            id: channelId,
+            name: user.name
+        };
+        this.props.setCurrentChannel(channelData);
+        this.props.setPrivateChannel(true);
+    }
+
+    getChannelId = userId => {
+        const currentUserId = this.state.user.uid;
+        return userId < currentUserId ? 
+            `${userId}/${currentUserId}` : `${currentUserId}/${userId}`;
+    }
     
     render() {
         const { users } = this.state
@@ -82,7 +99,7 @@ class DierctMessages extends React.Component {
                 {users.map(user => (
                     <Menu.Item
                         key={user.uid}
-                        onClick={() => console.log(user)}
+                        onClick={() => this.changeChannel(user)}
                         style={{ opacity: 0.7, fontstyle: 'italic'}}
                     >
                         <Icon 
@@ -97,4 +114,4 @@ class DierctMessages extends React.Component {
     }
 }
 
-export default DierctMessages;
+export default connect(null, { setCurrentChannel, setPrivateChannel })(DierctMessages);
