3ef26a4e06b71b11c8195379fee4c38a1fd82873
Yanik Kumar <believemasters@gmail.com>
2020-03-07 17:13:42 +0530

    Added Typing feature to show user is typing

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -5,6 +5,7 @@ import { setUserPosts } from '../../actions';
 import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
 import Message from './Message';
+import Typing from './Typing';
 
 import firebase from '../../firebase';
 
@@ -23,7 +24,10 @@ class Messages extends React.Component {
         numUniqueUsers: '',
         searchTerm: '',
         searchLoading: false,
-        searchResults: []
+        searchResults: [],
+        typingRef: firebase.database().ref('typing'),
+        typingUsers: [],
+        connectedRef: firebase.database().ref('.info/connected')
     }
 
     componentDidMount() {
@@ -37,6 +41,43 @@ class Messages extends React.Component {
 
     addListeners = channelId => {
         this.addMessageListner(channelId);
+        this.addTypingListeners(channelId);
+    }
+
+    addTypingListeners = channelId => {
+        let typingUsers = [];
+        this.state.typingRef.child(channelId).on('child_added', snap => {
+            if(snap.key !== this.state.user.uid) {
+                typingUsers = typingUsers.concat({
+                    id: snap.key,
+                    name: snap.val()
+                })
+                this.setState({ typingUsers });
+
+            }
+        })
+
+        this.state.typingRef.child(channelId).on('child_removed', snap => {
+            const index = typingUsers.findIndex(user => user.id === snap.key);
+            if(index !== -1) {
+                typingUsers = typingUsers.filter(user => user.id !== snap.key);
+                this.setState({ typingUsers });
+            }
+        })
+
+        this.state.connectedRef.on('value', snap => {
+            if(snap.val() === true) {
+                this.state.typingRef
+                    .child(channelId)
+                    .child(this.state.user.uid)
+                    .onDisconnect()
+                    .remove(err => {
+                        if(err !== null) {
+                            console.error(err)
+                        }
+                    })
+            }
+        })
     }
 
     addMessageListner = channelId => {
@@ -175,9 +216,17 @@ class Messages extends React.Component {
         return channel ? `${this.state.privateChannel ? '@' : '#'}${channel.name}` : '';
     }
 
+    displayTypingUsers = users => (
+        users.length > 0 && users.map(user => (
+            <div style={{display: "flex", alignItems: "center", marginBottom: "0.2em"}} key={user.id}>
+                <snan className="user__typing">{user.name} is typing</snan> <Typing />
+            </div>
+        ))
+    )
+
     render() {
         const { messagesRef, messages, channel, user, progressBar, numUniqueUsers, searchTerm, 
-            searchResults, searchLoading, privateChannel, isChannelStarred } = this.state;
+            searchResults, searchLoading, privateChannel, isChannelStarred, typingUsers } = this.state;
         return(
             <React.Fragment>
                 <MessagesHeader 
@@ -193,6 +242,7 @@ class Messages extends React.Component {
                 <Segment>
                     <Comment.Group className={progressBar ? "messages__progress" : "messages"}>
                         { searchTerm ? this.displayMessages(searchResults) : this.displayMessages(messages) }
+                        {this.displayTypingUsers(typingUsers)}
                     </Comment.Group>
                 </Segment>
 
